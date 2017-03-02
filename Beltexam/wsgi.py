85c35225ed53e3c62d3b9bf59d8aa64548b15107
taesung153 <taesung153@gmail.com>
2017-03-02 14:03:23 -0800

    Trip

--- Beltexam/wsgi.py
+++ Beltexam/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belt_round3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Beltexam.settings")
 
 application = get_wsgi_application()
